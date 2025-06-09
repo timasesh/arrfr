473e37cd77fb7973a86ea17e4b500fe87681cb75
Aibek <aibek.aibek.1204@gmail.com>
2025-06-09 11:39:54 +0500

    Final version 4

--- courses/urls.py
+++ courses/urls.py
@@ -77,4 +77,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
