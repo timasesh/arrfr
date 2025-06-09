6a3612002d6823511dac65170e2e14a435b4e5c8
Aibek <aibek.aibek.1204@gmail.com>
2025-06-09 10:06:53 +0500

    Final deploy version

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_courses.settings')
 
-application = get_wsgi_application()
+application = get_wsgi_application()    
